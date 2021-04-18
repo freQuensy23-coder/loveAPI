d9a315bbceae7e7db8a3afd04ed4a92056097cae
Alex <mametevalex@gmail.com>
2021-04-18 11:31:24 +0300

    Added new test

--- tests.py
+++ tests.py
@@ -7,6 +7,11 @@ class Tester(TestCase):
     def setUp(self) -> None:
         self.api = API()
         logging.basicConfig(level=logging.DEBUG)
+
     def test_get_random_msg(self):
         msg = self.api.get_random_msg(endpoints=[ENDPOINTS["her"]["quit"], ENDPOINTS["her"]["18+"]])
         print(msg)
+
+    def test_get_all_msgs(self):
+        msgs = self.api.get_all_msgs_group(endpoint=ENDPOINTS["him"]["morning"])
+        print(msgs[0])
