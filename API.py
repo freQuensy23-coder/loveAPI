e76ebbc8e82a41932765416c53ef3dfcce6c244a
Alex <mametevalex@gmail.com>
2021-04-18 09:33:47 +0300

    Add get random msg (from list of endpoints)

--- API.py
+++ API.py
@@ -5,15 +5,21 @@ from logging import getLogger
 class API:
     def __init__(self):
         self._log = getLogger("API")
+        self.endpoints = []
+        from parser import get_group_msgs
 
-    def get_all_msgs(self, endpoint):
+    def get_all_msgs_group(self, endpoint):
         self._log.debug(f"Getting all msgs from group {endpoint}")
         msgs = get_group_msgs(endpoint)
         self._log.debug(f"Get {msgs}")
         return msgs
 
-    def get_random_msg(self, endpoint):
+    def get_random_msg_group(self, endpoint):
         self._log.debug(f"Getting random msg from group {endpoint}")
         msg = choice(self.get_all_msgs(endpoint))
         self._log.debug(f"Get {msg}")
         return msg
+
+    def get_random_msg(self, endpoints):
+        endpoint = choice(endpoints)
+        self.get_all_msgs_group(endpoint)
