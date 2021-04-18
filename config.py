45aada933db5bd73879e050314a91c5051e5e134
Alex <mametevalex@gmail.com>
2021-04-18 09:18:04 +0300

    Add more endpoints

--- config.py
+++ config.py
@@ -3,7 +3,19 @@ domain = "http://smsta.ru/"
 
 ENDPOINTS = {
     "her":{
-        "love":"http://smsta.ru/m/sms/m_love"
+        "love":"http://smsta.ru/m/sms/m_love",
+        "fun":"http://smsta.ru/m/sms/m_fun",
+        "miss":"https://smsta.ru/m/sms/m_miss",
+        "morning":"https://smsta.ru/m/sms/m_sun",
+        "night":"https://smsta.ru/m/sms/m_night",
+        "18+":"https://smsta.ru/m/sms/m_erotic",
+        "18+poem":"https://smsta.ru/poems/passion",
+        "romance":"https://smsta.ru/m/romance",
+        "compliments":"https://smsta.ru/m/say",
+        "say love":"https://smsta.ru/m/feelings",
+        "quit":"https://smsta.ru/m/quit",
+        "sorry":"https://smsta.ru/m/sms/m_sorry"
+
     },
     "him":{
       "love":"http://smsta.ru/w/sms/love"
