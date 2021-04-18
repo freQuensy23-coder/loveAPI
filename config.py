93f4181eda9ae366fcbaa093607c74b04faae3ce
Alex <mametevalex@gmail.com>
2021-04-18 09:23:14 +0300

    Add more endpoints

--- config.py
+++ config.py
@@ -10,14 +10,26 @@ ENDPOINTS = {
         "night":"https://smsta.ru/m/sms/m_night",
         "18+":"https://smsta.ru/m/sms/m_erotic",
         "18+poem":"https://smsta.ru/poems/passion",
-        "romance":"https://smsta.ru/m/romance",
+        "romantic":"https://smsta.ru/m/romance",
         "compliments":"https://smsta.ru/m/say",
         "say love":"https://smsta.ru/m/feelings",
         "quit":"https://smsta.ru/m/quit",
         "sorry":"https://smsta.ru/m/sms/m_sorry"
-
     },
     "him":{
-      "love":"http://smsta.ru/w/sms/love"
+        "love":"http://smsta.ru/w/sms/love",
+        "fun":"https://smsta.ru/w/sms/fun",
+        "miss":"https://smsta.ru/w/sms/fun",
+        "night":"https://smsta.ru/w/sms/night",
+        "18+":"https://smsta.ru/w/sms/erotic",
+        "morning":"https://smsta.ru/w/sms/sun",
+        "romantic":"https://smsta.ru/w/romantic",
+        "compliments": "https://smsta.ru/w/sugar",
+        "say love":"https://smsta.ru/w/soul",
+        "poems":"https://smsta.ru/poems/amour",
+        "hurt":"https://smsta.ru/w/sms/obida",
+        "quit":"https://smsta.ru/w/bye",
+        "back":"https://smsta.ru/w/sms/start",
+        "sorry":"https://smsta.ru/w/sms/sorry"
     }
 }
